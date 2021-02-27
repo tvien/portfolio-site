a55a50ce3c32842b9db8136456ef63cc29118d4d
tvien <tonyxvien@gmail.com>
2021-02-27 18:00:40 -0500

    updated dependency

--- src/hooks/useScrollTopChecker.ts
+++ src/hooks/useScrollTopChecker.ts
@@ -4,14 +4,13 @@ import { useState, useEffect, useCallback } from 'react'
  * Description: Checks if the window scroll top position is passed the 'offset'
  */
 const useScrollTopChecker = (threshold: number = 0): boolean => {
-	const scrollY = typeof window !== `undefined` ? window.scrollY : 0
-
 	const [scrolledPastActionBar, setScrolledPastActionBar] = useState(
-		scrollY > threshold
+		(window || { scrollY: 0 }).scrollY > threshold
 	)
 
 	const checkScrollPos = useCallback(
-		() => setScrolledPastActionBar(scrollY > threshold),
+		() =>
+			setScrolledPastActionBar((window || { scrollY: 0 }).scrollY > threshold),
 		[threshold]
 	)
 
